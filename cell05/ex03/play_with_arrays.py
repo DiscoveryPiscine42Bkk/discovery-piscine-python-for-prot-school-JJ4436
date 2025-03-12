3ac36cd7c239d970b5f8f8a6f53e37b4637da2ac
JJ4436 <phisuththiphumihmuda@gmail.com>
2025-03-12 15:23:21 +0700

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,8 +1,10 @@
 array = [2, 8, 9, 48, 8, 22, -12, 2]
 new_array = set()  # ใช้ set เพื่อลบค่าซ้ำ
 
-for plus in array:
-    if plus > 5:
-        new_array.add(plus + 2)  # เพิ่มค่าที่ไม่ซ้ำเข้า set
+i = 0
+while i < 8:
+    if array[i] > 5:
+        new_array.add(array[i] + 2)  # เพิ่มค่าที่ไม่ซ้ำเข้า set
+    i += 1
 print(array)
 print(new_array)
