77bb5a5a7c2cc22383b75848919a4a618efdccbe
JJ4436 <phisuththiphumihmuda@gmail.com>
2025-03-12 14:59:58 +0700

    Update play_with_arrays.py

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,6 +1,8 @@
 array = [2, 8, 9, 48, 8, 22, -12, 2]
 new_array = []
-for plus in array:
-    new_array.append(plus + 2)
+i = 0
+while i < 8:
+    new_array.append(array[i] + 2)
+    i += 1
 print(f"Original array: {array}")
 print(f"New array: {new_array}")
