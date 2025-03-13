3eb066f678deed246ff898bfc65346f1ff666c68
JJ4436 <phisuththiphumihmuda@gmail.com>
2025-03-13 10:52:38 +0700

    Update count_it.py

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -1,8 +1,11 @@
-word = input()
-word = word.split()
+word = input().split()
+
 if len(word) == 0:
     print("none")
 else:
     print(f"parameter: {len(word)}")
-    for check in range(len(word)):
+    check = 0
+    while check < len(word):
+        word[check]= word[check].replace("'","").replace('"',"")
         print(f"{word[check]}: {len(word[check])}")
+        check += 1
