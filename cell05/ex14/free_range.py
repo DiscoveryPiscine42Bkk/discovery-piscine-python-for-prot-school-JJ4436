846f3fe138f2a807d63192af286d2a434ceb26d0
JJ4436 <phisuththiphumihmuda@gmail.com>
2025-03-13 10:40:06 +0700

    Update free_range.py

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,4 +1,3 @@
-
 number_input = input().split()
 
 if len(number_input) != 2:
@@ -8,7 +7,12 @@ else:
         start = int(number_input[0])
         end = int(number_input[1])
         
-        numbers = [i for i in range(start, end + 1)]
+        numbers = []
+        current = start
+        while current <= end:
+            numbers.append(current)
+            current += 1
+
         print(numbers)
     except:
         print("none")
